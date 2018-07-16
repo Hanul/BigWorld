c68ed8de9e72c4f98256fa0f9f77cafd595244de
YJ <hanul@hanul.me>
2018-07-17 00:53:43 +0900

    no message

--- BigWorld/BROWSER/Component/CreateSelectObjectPopup.js
+++ BigWorld/BROWSER/Component/CreateSelectObjectPopup.js
@@ -3,7 +3,12 @@ BigWorld.CreateSelectObjectPopup = METHOD({
 	run : (callback) => {
 		//REQUIRED: callback
 		
-		let tree;
+		let folderOpenedStore = BigWorld.STORE('folderOpenedStore');
+		
+		let selectedItem;
+		let selectedObjectId;
+		
+		let rootList;
 		let filenameInput;
 		SkyDesktop.Confirm({
 			okButtonTitle : '불러오기',
@@ -26,7 +31,7 @@ BigWorld.CreateSelectObjectPopup = METHOD({
 					fontWeight : 'bold'
 				},
 				c : '스테이지에 놓을 객체를 선택해주세요.'
-			}), tree = SkyDesktop.FileTree({
+			}), SkyDesktop.FileTree({
 				style : {
 					border : '1px solid #999',
 					marginTop : 8,
@@ -34,7 +39,6 @@ BigWorld.CreateSelectObjectPopup = METHOD({
 					padding : 8,
 					borderRadius : 4,
 					textAlign : 'left',
-					color : '#fff',
 					onDisplayResize : (width, height) => {
 						
 						if (height > 500) {
@@ -47,12 +51,178 @@ BigWorld.CreateSelectObjectPopup = METHOD({
 							};
 						}
 					}
-				}
+				},
+				c : rootList = SkyDesktop.Folder({
+					style : {
+						cursor : 'pointer'
+					},
+					icon : IMG({
+						src : BigWorld.R('explorer/drive.png')
+					}),
+					title : 'root',
+					on : {
+						open : (e, item) => {
+							item.setIcon(IMG({
+								src : BigWorld.R('explorer/drive.png')
+							}));
+						},
+						close : (e, item) => {
+							item.setIcon(IMG({
+								src : BigWorld.R('explorer/drive.png')
+							}));
+						},
+						tap : (e, item) => {
+							
+							if (selectedItem !== undefined) {
+								selectedItem.deselect();
+							}
+							item.select();
+							selectedItem = item;
+							
+							selectedObjectId = undefined;
+						}
+					}
+				})
 			})]
 		}, () => {
 			
+			if (selectedObjectId === undefined) {
+				return false;
+			}
+			
+			else {
+				//TODO:
+			}
 		});
 		
+		let loadFolders = (parentList, folderId) => {
+			
+			let list = folderId === undefined ? rootList : parentList;
+			
+			// 폴더들을 불러옵니다.
+			let foldersWatchingRoom = BigWorld.FolderModel.onNewAndFindWatching({
+				properties : {
+					folderId : folderId === undefined ? TO_DELETE : folderId
+				},
+				sort : {
+					createTime : 1
+				}
+			}, (folderData, addUpdateHandler, addRemoveHandler) => {
+				
+				let item;
+				list.addItem({
+					key : folderData.id,
+					item : item = SkyDesktop.Folder({
+						style : {
+							cursor : 'pointer'
+						},
+						isOpened : folderOpenedStore.get(folderData.id),
+						icon : IMG({
+							src : BigWorld.R('explorer/folder.png')
+						}),
+						title : folderData.name,
+						on : {
+							open : (e, item) => {
+								
+								folderOpenedStore.save({
+									name : folderData.id,
+									value : true
+								});
+								
+								loadFolders(item, folderData.id);
+							},
+							close : (e, item) => {
+								
+								folderOpenedStore.remove(folderData.id);
+								
+								item.removeAllItems();
+							},
+							tap : () => {
+								
+								if (selectedItem !== undefined) {
+									selectedItem.deselect();
+								}
+								item.select();
+								selectedItem = item;
+								
+								selectedObjectId = undefined;
+							}
+						}
+					})
+				});
+				
+				if (selectedItem !== undefined) {
+					selectedItem.deselect();
+				}
+				item.select();
+				selectedItem = item;
+				
+				addUpdateHandler((newFolderData) => {
+					item.setTitle(MSG(newFolderData.name));
+				});
+				
+				addRemoveHandler(() => {
+					list.removeItem(folderData.id);
+				});
+				
+				// 객체들을 불러옵니다.
+				let objectsWatchingRoom = BigWorld.ObjectModel.onNewAndFindWatching({
+					properties : {
+						folderId : folderId === undefined ? TO_DELETE : folderId
+					},
+					sort : {
+						createTime : 1
+					}
+				}, (objectData, addUpdateHandler, addRemoveHandler) => {
+					
+					let item;
+					list.addItem({
+						key : objectData.id,
+						item : item = SkyDesktop.File({
+							style : {
+								cursor : 'pointer'
+							},
+							icon : IMG({
+								src : BigWorld.R('explorer/object.png')
+							}),
+							title : MSG(objectData.name),
+							on : {
+								tap : () => {
+									
+									if (selectedItem !== undefined) {
+										selectedItem.deselect();
+									}
+									item.select();
+									selectedItem = item;
+									
+									selectedObjectId = objectData.id;
+								}
+							}
+						})
+					});
+					
+					addUpdateHandler((newObjectData) => {
+						item.setTitle(MSG(newObjectData.name));
+					});
+					
+					addRemoveHandler(() => {
+						item.remove();
+					});
+				});
+				
+				list.on('remove', () => {
+					objectsWatchingRoom.exit();
+					objectsWatchingRoom = undefined;
+				});
+			});
+			
+			list.on('remove', () => {
+				foldersWatchingRoom.exit();
+				foldersWatchingRoom = undefined;
+			});
+		};
 		
+		rootList.open();
+		loadFolders();
 	}
 });
